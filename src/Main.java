da532baf8452be060933c05c37b9187a68d2d9ee
CarolinaBolivar5 <cbolivar12@cesde.net>
2025-05-07 18:59:10 -0500

    Evidencia desempeño

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String opcion = "";
+        System.out.println("Menu de opciones");
+        System.out.println("Compra libros fisicos");
+        System.out.println("compra librosdigitales");
+        System.out.println("suscripcion anual");
+        switch (opcion);
+    }
+}
