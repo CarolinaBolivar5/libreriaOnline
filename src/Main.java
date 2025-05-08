666e557adeea20d6a130cb8f7d88209063d18afb
CarolinaBolivar5 <cbolivar12@cesde.net>
2025-05-07 19:33:41 -0500

    primer caso

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String opcion = "";
-        System.out.println("Menu de opciones");
-        System.out.println("Compra libros fisicos");
-        System.out.println("compra librosdigitales");
-        System.out.println("suscripcion anual");
-        switch (opcion);
+        System.out.println(" Menu de opciones");
+        System.out.println("1. Compra libros fisicos");
+        System.out.println("2. compra libros digitales");
+        System.out.println("3. suscripcion anual");
+        String opcion = sc.nextLine();
+        double cantidad = 0;
+        double precioLibro = 10000;
+        double total = 0;
+        double descuentoa = 0;
+        double descuentob = 0;
+
+        switch (opcion){
+            case "1":
+                System.out.println("Ingrese cantidad de libros ");
+                cantidad = sc.nextDouble();
+                total = cantidad*precioLibro;
+
+                if (cantidad>5){
+                    descuentoa=0.20;
+                }else if (cantidad>=3){
+                    descuentob=0.10;
+                }
+                double totalFinal = total*(1-(descuentoa+descuentob));
+                System.out.println(totalFinal);
+        }
     }
 }
